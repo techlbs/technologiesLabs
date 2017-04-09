3a77dd5f26485d5e66a69b3961ac9f05c9cff7d8
Artyom Yeremenko <eremenko_aa@groupbwt.com>
2017-04-09 21:19:51 +0300

    add autoprefixer

--- lab6/gulpfile.js
+++ lab6/gulpfile.js
@@ -1,22 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
 
 const path = {
     scss: './sass/**/*.scss',
+    css: './css/**/*.css',
     folders: {
         css: './css'
     }
 };
 
-gulp.task('default', function() {
-    watch(path.scss, function() {
-        gulp.start('sass');
+gulp.task('default', function () {
+    watch(path.scss, function () {
+        gulp.start(['sass']);
     });
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src(path.scss)
         .pipe(sass())
+        .pipe(gulp.dest(path.folders.css));
+});
+
+gulp.task('autoprefix', function () {
+    return gulp.src(path.css)
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions']
+        }))
         .pipe(gulp.dest(path.folders.css));
 });
