e28e6eee27c6cb0a3b65e127ceb14af0dc9879f3
Artyom Yeremenko <eremenko_aa@groupbwt.com>
2017-04-12 21:17:46 +0300

    init

--- lab6/js/yeremenko-slider.js
+++ lab6/js/yeremenko-slider.js
@@ -1,27 +1,62 @@
 const width = 1215;
-const fluency = 5; //плавность
-const speed = 5; //скорость
-const delay = 3000 + width; //задержка
-const direction = -1; //направление (1: слева на право; -1: справа на лево)
+const delay = 3000;
+const speed = '0.7s';
+const direction = -1;
+const image_blocks = 3;
+var page = 1;
+
+var t = setInterval(mv, delay);
+
+function mv() {
+    if(direction === -1) {//right to left
+        if(page >= image_blocks) {
+            setMargin(0);
+            page = 1;
+        } else {
+            rightToLeft();
+        }
+    } else {
+        if(page <= 1) {
+            setMargin(-1*(image_blocks - 1) * width);
+            page = image_blocks;
+        } else {
+            leftToRight();
+        }
+    }
+}
+
+function getStyleVal(slider, key) {
+    var cs = window.getComputedStyle(slider);
+    var tmp = cs.getPropertyValue(key);
+    return parseFloat(tmp);
+}
 
 function move(val) {
     var slider = document.getElementById('slider');
-    var cs = window.getComputedStyle(slider);
-    var tmp = cs.getPropertyValue('margin-left');
-    tmp = parseInt(tmp);
-    slider.style.marginLeft = (tmp + (val * direction)) + 'px';
+    var tmp = getStyleVal(slider, 'margin-left');
+    slider.style.transitionDuration = speed;
+    slider.style.marginLeft = (tmp + val) + 'px';
 }
-var t = setInterval(function() {
-    var moved = 0;
-    t2 = setInterval(function() {
-        if(moved >= width) {
-            clearInterval(t2);
-        }
-        var step = fluency;
-        if(moved + fluency > width) {
-            step = moved + fluency - width;
-        }
-        moved += step;
-        move(step);
-    },speed);
-}, delay);
+
+function rightToLeft() {
+    if (page < image_blocks) {
+        move(width * -1);
+        page++;
+        clearInterval(t);
+        t = setInterval(mv, delay);
+    }
+}
+function leftToRight() {
+    if (page > 1) {
+        move(width);
+        page--;
+        clearInterval(t);
+        t = setInterval(mv, delay);
+    }
+}
+
+function setMargin(val) {
+    var slider = document.getElementById('slider');
+    slider.style.transitionDuration = speed;
+    slider.style.marginLeft = val+'px';
+}
